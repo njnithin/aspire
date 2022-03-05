9e94c3848607a97cd2344be1f7595c79100de86a
Nithin Nj <n.j.nithin7@gmail.com>
2022-03-05 23:47:34 +0530

    Components and pages added

---
+++ src/components/AppMenu/AppMenu.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/card">Card</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style  lang="scss">
+</style>
---
+++ src/pages/UserCards/UserCards.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card-panel">
+    <h1>Card View</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardView'
+}
+</script>
+<style scoped lang="scss">
+
+</style>
